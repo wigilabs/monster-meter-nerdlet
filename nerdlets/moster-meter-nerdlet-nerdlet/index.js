bb729230f719d057cb5c31d58022b894ba80d2bd
Dieter Evangelista <hansevangelista@gmail.com>
2020-02-18 11:56:40 -0500

    first commit

---
+++ nerdlets/moster-meter-nerdlet-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class MosterMeterNerdletNerdletNerdlet extends React.Component {
+    render() {
+        return <h1>Hello, moster-meter-nerdlet-nerdlet Nerdlet!</h1>;
+    }
+}
